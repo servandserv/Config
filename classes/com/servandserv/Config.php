1031423a5a78a6e8e138c22e6172321f683bd359
Vladislav Kolpakov <servandserv@gmail.com>
2017-01-19 14:08:24 +0000

    initial commit

---
+++ classes/com/servandserv/Config.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace com\servandserv;
+
+abstract class Config
+{
+    private static $instance;
+    private $env = [];
+    
+    public static function getInstance()
+    {
+        if(!self::$instance) {
+            self::$instance = new self();
+        }
+        
+        return self::$instance;
+    }
+    
+    public function get( $prop, array $args = [] )
+    {
+        if( isset( $this->env[$prop] ) ) {
+            return $this->env[$prop];
+        } else if( isset( $_SERVER[$prop] ) ) {
+            return $_SERVER[$prop];
+        } else {
+            error_log( "Config property ".$prop." not found in configuration data in ".__FILE__." on ".__LINE__ );
+            exit();
+        }
+    }
+    
+    public function has( $prop )
+    {
+        if( isset( $_SERVER[$prop] ) || isset( $this->env[$prop] ) ) return TRUE;
+        else return FALSE;
+    }
+    
+    public function set( $prop, $val )
+    {
+        $_SERVER[$prop] = $val;
+    }
+    
+    public function create( $prop, array $args = [], callable $cb = NULL ) 
+    {
+        if( isset( $this->env[$prop] ) ) {
+            $isInvokable = is_object( $this->env[$prop] ) && method_exists( $this->env[$prop], "__invoke" );
+            if( $isInvokable ) $obj = call_user_func( $this->env[$prop], $args );
+            else $obj = $this->env[$prop];
+            if( $cb ) {
+                return call_user_func_array( $cb, array( $obj ) );
+            } else {
+                return $obj;
+            }
+        } else if( isset( $_SERVER[$prop] ) ) {
+            if( !class_exists( $_SERVER[$prop] ) ) throw new \Exception( "Environment interface implementation class \"$prop\" defined as ".$_SERVER[$prop]." not exists" );
+            $cl = new \ReflectionClass( $_SERVER[$prop] );
+            $obj = call_user_func_array( array( &$cl, 'newInstance' ), $args );
+            
+            return $obj;
+        }
+        throw new \Exception( "Environment interface \"$prop\" not exists" );
+    }
+}
